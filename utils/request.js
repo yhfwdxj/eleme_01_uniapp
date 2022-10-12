a867f5df4a5358a576836225eff8ca15cff63866
yhfwdxj <954405232@qq.com>
2022-10-12 21:39:50 +0800

    封装了request

---
+++ utils/request.js
@@ -0,0 +1,18 @@
+const baseUrl = 'https://elm.cangdu.org/v2/'
+
+export const request = (options) => {
+  return new Promise((resolve, reject) => {
+    uni.request({
+      url: baseUrl + options.url,
+      method: 'get' || options.method,
+      data: options.data || {},
+      header: options.header || {},
+      success: (res) => {
+        return resolve(res.data)
+      },
+      fail(err) {
+        return reject('err')
+      }
+    })
+  })
+}
