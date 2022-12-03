21f7263858e34042737b4123c8176b80aab7f432
yhfwdxj <954405232@qq.com>
2022-12-04 01:32:20 +0800

    用户界面

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -1,8 +1,103 @@
 <template>
+  <view class="user-container">
+    <text style="margin: 3%;">用户信息:</text>
+    <view class="user" style="margin-top: 3%;">
+      <view class="user-img">
+        <button class="login-btn" open-type="chooseAvatar" @chooseavatar="chooseavatar">
+          <text>头像</text>
+          <view class="profile">
+            <img :src="avatarUrl" class="profile-img">
+          </view>
+        </button>
+      </view>
+      <view class="user-img">
+        <button class="login-btn">
+          <text>昵称</text>
+          <view class="input-name">
+            <input type="nickname" placeholder="请输入昵称">
+          </view>
+        </button>
+      </view>
+      <view class="user-img">
+        <button class="login-btn">
+          <text>个人简介</text>
+          <view class="profile">
+            <input type="text">
+          </view>
+        </button>
+      </view>
+      <view class="user-img">
+        <button class="login-btn" @click="goSearch">
+          <text>收货地址</text>
+          <view class="profile">
+            <img :src="avatarUrl" class="profile-img">
+          </view>
+        </button>
+      </view>
+    </view>
+  </view>
 </template>
 
-<script>
+<script setup>
+  import {
+    ref
+  } from 'vue'
+  let avatarUrl = ref('')
+  const chooseavatar = (e) => {
+    avatarUrl.value = e.detail.avatarUrl
+    console.log(avatarUrl);
+  }
+  const goSearch = () => {
+    uni.navigateTo({
+      url: '/subpkg/location/location'
+    })
+  }
 </script>
+<style lang="scss">
+  button::after {
+    border: none;
+  }
 
-<style>
+  .user-container {
+
+    .user {
+      .user-img {
+
+        .login-btn {
+          position: relative;
+          width: 95%;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          height: 110rpx;
+          font-size: 30rpx;
+          background-color: #fff;
+          border-top: 2rpx solid rgb(200, 200, 200);
+
+          .input-name {
+            position: absolute;
+            right: 5%;
+            text-align: right;
+          }
+
+          .profile {
+            display: flex;
+            align-items: center;
+
+            &::after {
+              content: '>';
+            }
+
+            .profile-img {
+              width: 80rpx;
+              height: 60rpx;
+              margin-right: 10rpx;
+            }
+          }
+
+
+        }
+      }
+    }
+  }
 </style>
--- unpackage/dist/dev/mp-weixin/pages/user/user.js
+++ unpackage/dist/dev/mp-weixin/pages/user/user.js
@@ -1,8 +1,27 @@
 "use strict";
 var common_vendor = require("../../common/vendor.js");
-const _sfc_main = {};
-function _sfc_render(_ctx, _cache) {
-  return {};
-}
-var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "E:/Study/myWork/eleme_01_uniapp/pages/user/user.vue"]]);
+const _sfc_main = {
+  __name: "user",
+  setup(__props) {
+    let avatarUrl = common_vendor.ref("");
+    const chooseavatar = (e) => {
+      avatarUrl.value = e.detail.avatarUrl;
+      console.log(avatarUrl);
+    };
+    const goSearch = () => {
+      common_vendor.index.navigateTo({
+        url: "/subpkg/location/location"
+      });
+    };
+    return (_ctx, _cache) => {
+      return {
+        a: common_vendor.unref(avatarUrl),
+        b: common_vendor.o(chooseavatar),
+        c: common_vendor.unref(avatarUrl),
+        d: common_vendor.o(goSearch)
+      };
+    };
+  }
+};
+var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__file", "E:/Study/myWork/eleme_01_uniapp/pages/user/user.vue"]]);
 wx.createPage(MiniProgramPage);
