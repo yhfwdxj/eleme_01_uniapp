71777d1f0795a4dfd0740f5a04b5168cabcf6bfc
yhfwdxj <954405232@qq.com>
2023-05-06 07:15:25 +0800

    remove unpackage

--- unpackage/dist/dev/mp-weixin/utils/message.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const common_vendor = require("../common/vendor.js");
-const loading = () => {
-  return common_vendor.index.showToast({
-    title: "加载中",
-    icon: "loading"
-  });
-};
-exports.loading = loading;
